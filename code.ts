8b6288210c1f28ae67771ea77d16fa058f36237f
Christine Parsons <christine@pagesandsheets.com>
2025-08-04 14:51:51 -0400

    add menu with alert

---
+++ code.ts
@@ -0,0 +1,21 @@
+const onOpen = () => {
+  SpreadsheetApp.getUi()
+    .createMenu("NEW MENU!!")
+    .addItem("Show Alert", "showAlert")
+    .addToUi();
+};
+
+const showAlert = () => {
+  const ui = SpreadsheetApp.getUi();
+  const result = ui.alert(
+    "Please Confirm",
+    "Are you sure you want to continue?",
+    ui.ButtonSet.YES_NO
+  );
+
+  if (result == ui.Button.YES) {
+    ui.alert("Confirmation Recieved");
+  } else {
+    ui.alert("Permission Denied");
+  }
+};
